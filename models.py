2cfbd5f26e91fedb4b9dd12b9c7dd6267e129117
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-07-30 20:16:20 +0100

    Creation of Base Class for All models

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
-from config import *
+from config import db
 
 from sqlalchemy import inspect
 
 
-
-
-class Base:
+class Utilities:
+    """
+    Utilities function for All Model
+    """
     def asdict(self) -> dict:
         """
         Return a dictionary representation of the model.
@@ -14,7 +15,22 @@ class Base:
                 for c in inspect(self).mapper.column_attrs}
 
 
-class Show(db.Model, Base):
+class Base(db.Model, Utilities):
+    """
+    Base class for Venue and Artist
+    """
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image_link = db.Column(db.String(1000))
+    facebook_link = db.Column(db.String(500))
+    website = db.Column(db.String(500))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.PickleType())
+    seeking_description = db.Column(db.TEXT())
+
+
+class Show(Utilities, db.Model):
     __tablename__ = 'show'
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
     artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
@@ -25,39 +41,23 @@ class Show(db.Model, Base):
                                                                                       "delete, delete-orphan"))
 
 
-class Venue(db.Model, Base):
+class Venue(Base):
     __tablename__ = 'Venue'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
     city = db.Column(db.String(120), nullable=False)
     state = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(1000))
-    facebook_link = db.Column(db.String(500))
-    website = db.Column(db.String(500))
     seeking_talent = db.Column(db.Boolean)
-    genres = db.Column(db.PickleType())
-    seeking_description = db.Column(db.TEXT())
 
     def __repr__(self):
         return f'<Venue {self.id} {self.name}>'
 
 
-class Artist(db.Model, Base):
+class Artist(Base):
     __tablename__ = 'Artist'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.PickleType())
-    image_link = db.Column(db.String(1000))
-    facebook_link = db.Column(db.String(500))
-    website = db.Column(db.String(500))
     seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.TEXT())
 
     def __repr__(self):
         return f'<Artist {self.id} {self.name}>'
